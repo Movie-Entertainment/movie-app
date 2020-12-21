76332869eb37fe01ed023f9b6d9f485418da7606
zmanan <73906206+zmanan@users.noreply.github.com>
2020-12-20 21:22:00 -0500

    Addded docstring to the test file.

--- test.py
+++ test.py
@@ -7,17 +7,30 @@ import ott
 
 
 def test_platform():
+    """This method will test the main script"""
     my_entertainment = Entertainment()
     assert my_entertainment.platform == None
     assert my_entertainment.result == None
 
 def test_youtube():
+    """This method will test the youtube script. First run the youtube script, then copy
+       and paste the results in the assert statement below. Finally run this test script. If 
+       successful, the message "Everything Passed" will be printed.
+    """
     assert youtube.lookup_youtube("peppermint")[0] == "Peppermint Trailer #1 (2018) | Movieclips Trailers"
 
 def imdb_id():
+    """This method will test the imdb script. First run the imdb script, then copy
+       and paste the results in the assert statement below. Finally run this test script. If 
+       successful, the message "Everything Passed" will be printed.
+    """
     assert imdb.imdb("predator")[0] == "Xtinction: Predator X"
 
 def test_ott():
+    """This method will test the ott script. First run the ott script, then copy
+       and paste the results in the assert statement below. Finally run this test script. If 
+       successful, the message "Everything Passed" will be printed.
+    """
     assert ott.ott("titanic")[0] == "Back to the Titanic"
 
 
